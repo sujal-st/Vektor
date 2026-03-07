a0286497f821536251e4ce622fa1c6a00126942b
sujal-st <sujal.sthapit4@gmail.com>
2026-03-07 18:00:06 +0545

    fix(api_endpoint): "api endpoint commit "

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['https://vektor-delta.vercel.app/'],
+    allow_origins=['https://vektor-delta.vercel.app'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
